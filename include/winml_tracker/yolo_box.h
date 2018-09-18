c279803829d1ad6131ddce109f7b707569ac3345
Stuart Schaefer <stscha@microsoft.com>
2018-09-18 14:01:10 -0700

    Add MarkerArray support.  Fix tabs and spaces.

--- include/winml_tracker/yolo_box.h
+++ include/winml_tracker/yolo_box.h
@@ -5,38 +5,38 @@
 
 namespace yolo
 {
-	const int ROW_COUNT = 13;
-	const int COL_COUNT = 13;
-	const int CHANNEL_COUNT = 125;
-	const int BOXES_PER_CELL = 5;
-	const int BOX_INFO_FEATURE_COUNT = 5;
-	const int CLASS_COUNT = 20;
-	const float CELL_WIDTH = 32;
-	const float CELL_HEIGHT = 32;
+    const int ROW_COUNT = 13;
+    const int COL_COUNT = 13;
+    const int CHANNEL_COUNT = 125;
+    const int BOXES_PER_CELL = 5;
+    const int BOX_INFO_FEATURE_COUNT = 5;
+    const int CLASS_COUNT = 20;
+    const float CELL_WIDTH = 32;
+    const float CELL_HEIGHT = 32;
 
-	static const std::string labels[CLASS_COUNT] =
-	{
-		"aeroplane", "bicycle", "bird", "boat", "bottle",
-		"bus", "car", "cat", "chair", "cow",
-		"diningtable", "dog", "horse", "motorbike", "person",
-		"pottedplant", "sheep", "sofa", "train", "tvmonitor"
-	};
+    static const std::string labels[CLASS_COUNT] =
+    {
+        "aeroplane", "bicycle", "bird", "boat", "bottle",
+        "bus", "car", "cat", "chair", "cow",
+        "diningtable", "dog", "horse", "motorbike", "person",
+        "pottedplant", "sheep", "sofa", "train", "tvmonitor"
+    };
 
-	struct YoloBox
-	{
-	public:
-		std::string label;
-		float x, y, width, height, confidence;
-	};
+    struct YoloBox
+    {
+    public:
+        std::string label;
+        float x, y, width, height, confidence;
+    };
 
-	class YoloResultsParser
-	{
-	public:
-		static std::vector<YoloBox> GetRecognizedObjects(std::vector<float> modelOutputs, float threshold = 0.3f);
-	private:
-		static int GetOffset(int x, int y, int channel);
-		static float IntersectionOverUnion(YoloBox a, YoloBox b);
-		static float Sigmoid(float value);
-		static void Softmax(std::vector<float> values);
-	};
+    class YoloResultsParser
+    {
+    public:
+        static std::vector<YoloBox> GetRecognizedObjects(std::vector<float> modelOutputs, float threshold = 0.3f);
+    private:
+        static int GetOffset(int x, int y, int channel);
+        static float IntersectionOverUnion(YoloBox a, YoloBox b);
+        static float Sigmoid(float value);
+        static void Softmax(std::vector<float> values);
+    };
 }
